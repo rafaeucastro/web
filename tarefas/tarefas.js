930c04b49cce5e10b32e5174c61fad0040b30172
Rafaeu Castro <castrorafael456@gmail.com>
2023-05-24 11:33:10 -0300

    Adicionado gerenciador tarefas

---
+++ tarefas/tarefas.js
@@ -0,0 +1,42 @@
+function addTask(){
+    var taskInput = document.getElementById("add-task");
+    var taskList = document.getElementById("task-list");
+
+    var task = document.createElement("li");
+
+    //delete button
+    var deleteIcon = document.createElement("i");
+    deleteIcon.className = "fa-light fa-delete-left";
+    var deleteButton = document.createElement("button");
+    deleteButton.append(deleteIcon);
+    deleteButton.onclick = function () {
+        
+    }
+
+    //label
+    var taskLabel = document.createElement("input");
+    taskLabel.value = taskInput.value;
+    taskLabel.style.border = "none";
+    taskLabel.style.backgroundColor = "transparent";
+    taskLabel.onclick = function (){
+        taskLabel.style.border = "default";
+        taskLabel.style.backgroundColor = "default";
+        task.appendChild(deleteButton);
+    }
+
+    //radio button
+    var radio = document.createElement("input");
+    radio.type = "radio";
+    radio.onclick = function () {
+        taskLabel.style.textDecoration = 'line-through';
+    }
+
+    //append elements
+    task.appendChild(radio);
+    task.appendChild(taskLabel);
+    taskList.appendChild(task);
+
+    //cleaning input
+    taskInput.value = "";
+}
+
